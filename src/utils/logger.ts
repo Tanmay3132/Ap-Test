fb8ecf5336b14c2901b1831b8f221c590d1fd611
sanketraut1 <sanket.raut@leadows.com>
2025-02-05 09:48:43 +0530

    [FEAT] : Added Loki logging

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,6 @@
-import { existsSync, mkdirSync } from 'fs';
-import { join } from 'path';
+import { LOG_GROUP_NAME, LOG_SERVICE_NAME, LOKI_URL } from '@config';
 import winston from 'winston';
-import winstonDaily from 'winston-daily-rotate-file';
-import { LOG_DIR } from '@config';
-
-// logs dir
-const logDir: string = join(__dirname, LOG_DIR);
-
-if (!existsSync(logDir)) {
-  mkdirSync(logDir);
-}
+import LokiTransport from 'winston-loki';
 
 // Define log format
 const logFormat = winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`);
@@ -27,25 +18,16 @@ const logger = winston.createLogger({
   ),
   transports: [
     // debug log setting
-    new winstonDaily({
-      level: 'debug',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/debug', // log file /logs/debug/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      json: false,
-      zippedArchive: true,
-    }),
-    // error log setting
-    new winstonDaily({
-      level: 'error',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/error', // log file /logs/error/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      handleExceptions: true,
-      json: false,
-      zippedArchive: true,
+    new LokiTransport({
+      host: LOKI_URL,
+      onConnectionError: err => console.error(`LOKI CONNECTION ERROR: ${err}`),
+      useWinstonMetaAsLabels: false,
+      batching:  true,
+      interval:  5,
+      labels: {
+        appname: LOG_GROUP_NAME,
+        service_name: LOG_SERVICE_NAME,
+      },
     }),
   ],
 });
